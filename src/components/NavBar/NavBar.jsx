53c49f5d51194ea6d2f99c0ab15bfbf1e8be5c6c
Abhinav Anand <abhinavanand072@gmail.com>
2026-08-17 10:24:31 +0530

    Added fonts

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import './NavBar.css'
+
+export default function NavBar() {
+    return (
+        <>
+            <div>
+                <p></p>
+                <div>
+                    <a></a>
+                    <a></a>
+                    <a></a>
+                    <a></a>
+                </div>
+                <button></button>
+            </div>
+        </>
+    )
+}
