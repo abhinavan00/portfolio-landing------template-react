9fdded4a7eef8080ca536e06e52ece77ba3b9799
Abhinav Anand <abhinavanand072@gmail.com>
2026-08-18 13:35:34 +0530

    hero section complete

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -4,20 +4,23 @@ import './HeroSection.css'
 export default function HeroSection() {
     return (
         <>
-            <section>
-                <div>
+            <section className='hero-section'>
+                <div className='hero-section-details'>
                     <h1> Building the future of web experiences </h1>
                     <p>
                         Full-stack developer specializing in scalable architecture, high-
                         performance interfaces, and AI integration. Crafting precise
                         solutions for complex problems.
                     </p>
-                    <div>
-                        <button>Hire Me</button>
-                        <button>View Projects</button>
+                    <div className='hero-section-btns-container'>
+                        <button className='hire-me-btn'>Hire Me</button>
+                        <button className='view-project-btn'>View Projects</button>
                     </div>
                 </div>
-                <img src={DeveloperProfile} alt='Profile Image of Developer' />
+                <div className='img-container'>
+                    <img src={DeveloperProfile} alt='Profile Image of Developer' />
+                    <div className='img-overlay'></div>
+                </div>
             </section>
         </>
     )
